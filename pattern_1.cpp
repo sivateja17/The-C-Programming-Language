a0101f8f9cb53ead6f78c30fc7afe119ea1892c3
sivateja17 <48089113+sivateja17@users.noreply.github.com>
2019-02-28 16:42:03 +0530

    Add files via upload

---
+++ pattern_1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int n,i,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		if(i%2==0){
+			for(j=n-i+1;j>0;j--){
+				printf("%d ",j);
+			}
+		}
+		else{
+			for(j=1;j<=n-i+1;j++){
+				printf("%d ",j);
+			}
+		}
+		printf("\n");
+	}
+}
